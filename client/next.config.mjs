0abc85cbbdc19f5df872f267fe19b78bd098ead6
sandipkatel <babusandipkatel@gmail.com>
2025-01-23 01:32:59 +0545

    fix build log

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // Use 'standalone' mode if deploying to a custom server
+};
 
 export default nextConfig;
