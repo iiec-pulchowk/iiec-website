38085ef1ab4547aa36300876e05de46f430c4b09
SijanJ <sijan2joshi@gmail.com>
2025-05-26 11:14:01 +0545

    added backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const app = require("./src/app.js");
 const connectDB = require("./src/database/database.js");
+require("dotenv").config();
 
 connectDB();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server started at ${process.env.PORT}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
 });
