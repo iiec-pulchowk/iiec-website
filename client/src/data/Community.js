f650ce714bde17bef16db85a3615047a0d625345
sandipkatel <babusandipkatel@gmail.com>
2025-05-31 19:03:02 +0545

    docs: remove bdf communty

--- client/src/data/Community.js
+++ client/src/data/Community.js
@@ -54,22 +54,6 @@ export const communityOrganizations = [
   },
   {
     id: 3,
-    name: "DBF",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
-    projects: [
-      {
-        name: "Project 1",
-        description:
-          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-      },
-    ],
-    image: "/community/dbf.png",
-    imageAlt: "Design Build Fly team with their aeroplane prototype",
-    url: "#",
-  },
-  {
-    id: 3,
     name: "Flying Club",
     description:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
